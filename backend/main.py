6b52072d96274090bb0ef3f31d601e39f49b4ce8
shahmimajid <im.shahmi@gmail.com>
2022-05-03 15:06:10 +0800

    Added for static content

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,22 @@
-from tracemalloc import start
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles #new
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
 
 def include_router(app):
-	app.include_router(general_pages_router)
+    app.include_router(general_pages_router)
+    
+def configure_static(app):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 def start_application():
-	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
-	include_router(app)
-	return app 
+    app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+    include_router(app)
+    configure_static(app)
+    return app
 
-app = start_application
+app = start_application()
 
 # @app.get("/")
 # def hello_api():
